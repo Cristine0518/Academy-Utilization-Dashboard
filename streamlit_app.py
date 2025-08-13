6bfbc56b53e4c7ec2c571be2a6849709c2a01278
Cristine0518 <ma.christine.volante@cognizant.com>
2025-08-13 17:47:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-streamlit
-pandas
-plotly
-openpyxl
+import streamlit as st
+
+st. title("Academy Utilization Dashboard")
+
+st.header('The Academy Utilization Dashboard App is an interactive data visualization tool built with Streamlit, designed to track and analyze utilization metrics for training and development activities. It provides a clear and engaging overview of how time is spent across various activity categories within the IOA Academy Phil.')
